9e31dd1914cdadfe9c21c3ce757511b9beb881b2
xd008 <2969316817@qq.com>
2019-06-18 11:19:13 +0800

    提交

--- app/src/main/java/com/xd/myzxinglib/MainActivity.java
+++ app/src/main/java/com/xd/myzxinglib/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        https://www.jianshu.com/p/b4f93946a27d
     }
 }
